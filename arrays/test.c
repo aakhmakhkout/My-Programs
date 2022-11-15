00006868b4a397ca00244c8aa9ee9f8648e7fd70
aakhmakhkout <117374518+aakhmakhkout@users.noreply.github.com>
2022-11-15 15:45:13 +0530

    Update test.c

--- arrays/test.c
+++ arrays/test.c
@@ -4,7 +4,8 @@ int main() {
     // int x=5;
     // printf("%p", &x);
     // return 0;
-
+    
+    // ARRAYS
     int test[10]={2,4,223,45,34,423342,43,32,342,6675};
     printf("%d, %d, %d, %d", test[8],test[3],test[3],test[5]);
     // printf("%d", test[4]);
